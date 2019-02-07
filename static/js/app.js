20202d3f70c9cf5b7520b9437d9a4ebce4828a53
Sarah Grabinski <sarah.grabinski@gmail.com>
2019-02-06 22:53:40 -0500

    Final cleanup

--- static/js/app.js
+++ static/js/app.js
@@ -7,101 +7,48 @@ var submit = d3.select("#filters");
 // Select the tbody
 var tbody = d3.select("tbody");
 
-// // Form Version
-// submit.on("click", function() {
-
-//   // Prevent the page from refreshing
-//   d3.event.preventDefault();
-
-//   // Select the input element and get the raw HTML node
-//   var inputElement = d3.select("#datetime");
-
-//   // Get the value property of the input element
-//   var inputValue = inputElement.property("value");
-
-//   console.log(inputValue);
-//   console.log(sightings);
-
-//   var filteredData = sightings.filter(sighting => sighting.datetime === inputValue);
-
-//   console.log(filteredData);
-
-//   filteredData.forEach(function(sighting) {
-//     console.log(sighting);
-//     var row = tbody.append("tr");
-//     Object.entries(sighting).forEach(function([key, value]) {
-//       console.log(key, value);
-//       // Append a cell to the row for each value
-//       // in the weather report object
-//       var cell = tbody.append("td");
-//       cell.text(value);
-//     });
-//   });
-// });
-
-// Dropdown options setup
-
+// Map dates & eliminate duplicates
 var dates = sightings.map(sighting => sighting.datetime);
 console.log(dates);
-
 function onlyUnique(value, index, self) { 
     return self.indexOf(value) === index;
 }
 var uniqueDates = dates.filter(onlyUnique);
 console.log(uniqueDates);
 
+// Add unique dates to select dropdown
 var dropdownBox = d3.select("#filters");
 uniqueDates.forEach(date => dropdownBox.append("option").text(date));
 
-// Dropdown Version
+// Dropdown Filter
 submit.on("change", function() {
 
     // Prevent the page from refreshing
     d3.event.preventDefault();
 
-    // Remove previous data
-    // tbody.removeAttribute("tr")
-
-    // Get the value property of the input element
+    // Get the value of the selection
     var inputValue = submit.property("value");
 
     console.log(inputValue);
     console.log(sightings);
 
+    // Filter data by selected date
     var filteredData = sightings.filter(sighting => sighting.datetime === inputValue);
-
     console.log(filteredData);
+
+    // Clear any rows from previous filters
     tbody.html("");
+
+    // Add rows to table
     filteredData.forEach(function(sighting) {
         console.log(sighting);
         var row = tbody.append("tr");
+
+        // Add cells to rows
         Object.entries(sighting).forEach(function([key, value]) {
             console.log(key, value);
-            // Append a cell to the row for each value
-            // in the weather report object
             var cell = row.append("td");
             cell.text(value);
         });
     });
 });
-
-//   // BONUS: Calculate summary statistics for the age field of the filtered data
-
-//   // First, create an array with just the age values
-//   var ages = filteredData.map(person => person.age);
-
-//   // Next, use math.js to calculate the mean, median, mode, var, and std of the ages
-//   var mean = math.mean(ages);
-//   var median = math.median(ages);
-//   var mode = math.mode(ages);
-//   var variance = math.var(ages);
-//   var standardDeviation = math.std(ages);
-
-//   // Finally, add the summary stats to the `ul` tag
-//   d3.select(".summary")
-//     .append("li").text(`Mean: ${mean}`)
-//     .append("li").text(`Median: ${median}`)
-//     .append("li").text(`Mode: ${mode}`)
-//     .append("li").text(`Variance: ${variance}`)
-//     .append("li").text(`Standard Deviation: ${standardDeviation}`);
-// });
